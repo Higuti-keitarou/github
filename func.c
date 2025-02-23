d570929c81a7f30219788c641faefba2fea07c76
Keitarou-Desktop <0530keitalou.jp@gmail.com>
2025-02-23 15:44:06 +0900

    README.mdに追記

---
+++ func.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+int sum (int i, int y)
+{
+    int z;
+    z = i + y;
+    printf("%d", z);
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+int main (void) 
+{
+    printf("hello,Git");
+    sum(10, 8);
+    return 0;
+}
